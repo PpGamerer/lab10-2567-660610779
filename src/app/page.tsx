70e28b947088d876ac3805c2616e6ab68f6fab89
Pp <gamerer16@gmail.com>
2024-08-05 18:21:52 +0700

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map((user: UserCardProps) => (<UserCard key={user.email} {...user}/>))} //
+      {users && !isLoading && users.map((user: UserCardProps) => (<UserCard key={user.email} {...user}/>))}
     </div>
   );
 }
